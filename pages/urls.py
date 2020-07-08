160033daf9037a7c83ed75f4d8e75a3ff1b6aa5a
Abdullah-0604 <tmilan0604@gmail.com>
2020-07-08 09:40:34 +0545

    Partially done

--- pages/urls.py
+++ pages/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("record/", views.record, name="record"),
-    path("province/", views.province, name="province"),
+    path("province1/", views.province1, name="province1"),
+    path("province2/", views.province2, name="province2"),
+    path("province3/", views.province3, name="province3"),
+    path("province4/", views.province4, name="province4"),
+    path("province5/", views.province5, name="province5"),
     path("visualization/", views.visualization, name="visualization"),
     path("feedback/", views.feedback, name="feedback"),
 
--- pages/views.py
+++ pages/views.py
@@ -9,14 +9,28 @@ def about(request):
 def record(request):
     return render(request,"food_vis/record.html",{})
 
-def province(request):
-    return render(request,"food_vis/province.html",{})
+def province1(request):
+    return render(request,"food_vis/province1.html",{})
+
+def province2(request):
+    return render(request,"food_vis/province2.html",{})
+    
+def province3(request):
+    return render(request,"food_vis/province3.html",{})
+    
+def province4(request):
+    return render(request,"food_vis/province4.html",{})
+    
+def province5(request):
+    return render(request,"food_vis/province5.html",{})
+    
 
 def visualization(request):
     return render(request,"food_vis/visualization.html",{})
 
 def feedback(request):
     return render(request,"food_vis/feedback.html",{})
+        
 
 
 
